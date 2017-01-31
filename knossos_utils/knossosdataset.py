3c5ad55323f13d519b2091b00fa647d90e05818c
chbehrens <c.behrens@uni-tuebingen.de>
2017-01-31 16:30:31 +0100

    allow no mag folder

--- knossos_utils/knossosdataset.py
+++ knossos_utils/knossosdataset.py
@@ -1484,9 +1484,13 @@ class KnossosDataset(object):
                     current[0] = start[0]
                     while current[0] < end[0]:
                         this_cube_info = []
-                        path = self._knossos_path+self._name_mag_folder + \
-                               str(mag) + "/" + "x%04d/y%04d/z%04d/" \
-                                        % (current[0], current[1], current[2])
+			if self._name_mag_foler == None:
+                            path = self._knossos_path + "x%04d/y%04d/z%04d/" \
+                                        % (current[0], current[1], current[2]) 
+                        else:
+                            path = self._knossos_path+self._name_mag_folder + \
+                                   str(mag) + "/" + "x%04d/y%04d/z%04d/" \
+                                            % (current[0], current[1], current[2])
 
                         this_cube_info.append(path)
 
